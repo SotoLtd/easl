c14b336fad9092d0f05707c92cc91482816fd92a
Mahbub <mmhasaneee@gmail.com>
2021-03-23 19:02:34 +0600

    Change version to 2021.03.17.02

--- themes/Total-Child/functions.php
+++ themes/Total-Child/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('EASL_THEME_VERSION', '2021.03.03.01');
+define('EASL_THEME_VERSION', '2021.03.17.02');
 //define( 'EASL_THEME_VERSION', time() );
 
 if ( ! defined( 'EASL_INC_DIR' ) ) {
