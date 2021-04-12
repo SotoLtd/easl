73f83a08f3dfa6cc6fe3a3c2abfabea2e0ecfc96
Mahbub <mmhasaneee@gmail.com>
2021-04-12 22:44:28 +0600

    Page header styling issue

--- themes/Total-Child/functions.php
+++ themes/Total-Child/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('EASL_THEME_VERSION', '2021.04.07.03');
+define('EASL_THEME_VERSION', '2021.04.07.05');
 //define( 'EASL_THEME_VERSION', time() );
 
 if ( ! defined( 'EASL_INC_DIR' ) ) {
